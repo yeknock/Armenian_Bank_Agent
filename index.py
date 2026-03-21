2d2cc2ae38b70985404f38071ebfaa6848f6e279
yeknock <yen.martirosyan@gmail.com>
2026-03-21 19:45:12 +0400

    Added embeddings in index.py

--- index.py
+++ index.py
@@ -7,18 +7,25 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 dirs = [
-    "acba/branches", "acba/credits", "acba/deposits",
-    "ameria/branches", "ameria/credits", "ameria/deposits",
-    "converse/branches", "converse/credits", "converse/deposits"
+    "acba/branches", #, "acba/credits", "acba/deposits",
+    "ameria/branches" #, "ameria/credits", "ameria/deposits",
+    #"converse/branches", "converse/credits", "converse/deposits"
     ]
 
 for dir in dirs:
     directory = Path("knowledge/" + dir)
     for filepath in directory.glob('*.md'):
         with open(filepath, "r", encoding="utf-8") as f:
-            response = client.embeddings.create(
-                input = f.read(),
-                model = "text-embedding-3-small"
-            )
+            words = f.read().split()
+            i = 0
+            while i < len(words):
+                chunk_list = words[i:i + 200]
+                chunk_str = ' '.join(chunk_list)
 
-            print(response.data[0].embedding)
+                response = client.embeddings.create(
+                    input = chunk_str,
+                    model = "text-embedding-3-small"
+                )
+
+                print(response.data[0].embedding)
+                i += 200
